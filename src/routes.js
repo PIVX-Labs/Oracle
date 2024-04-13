7b814e76da6db195e97e350bd44b51a1ba2f46f3
JSKitty <jskitty@protonmail.com>
2024-04-13 18:02:26 +0100

    Cleanup and refactor routes logic

--- src/routes.js
+++ src/routes.js
@@ -17,122 +17,109 @@ const dataSourceUpdateTime = { //listed in seconds
 const ROOT_PREFIX = process.env.ORACLE_ROOT_PREFIX || '';
 
 router.get(ROOT_PREFIX + '/api/v1/currencies', async(req, res) =>{
-    
-    //create marketData array
-    let marketData = []
-    //load marketData
-    const arrPersistentDataSource = await readDataSource();
-    for (const dataSource of arrPersistentDataSource) {
-        marketData.push(dataSource);
+    // Fetch market data from disk
+    const arrMarketData = await prepareMarketData();
+    if (arrMarketData.length == 0) {
+        console.error('Warning: Oracle has no data after multiple attempts, cannot provide data to API requests!');
+        return res.status(500).json({ err: "Oracle doesn't have enough data to respond, try again later!" });
     }
 
-    let response = []
-    const average = array => array.reduce((a, b) => a + b) / array.length;
-
-    //if marketData isn't set up
-    if(marketData.length == 0){
-        console.log("ran no db")
-        await getMarketData(marketData, 'coinGecko','usd');
-        await getMarketData(marketData, 'coinGeckoDirect', 'usd')
-        await getMarketData(marketData, 'binance','usd');
-        await getMarketData(marketData, 'coinMarketCap','usd');
-    }else{
-        //(list of possible currencies alongside the number)
-        marketData.forEach((marketDataLastChecked) => {
-            if(marketDataLastChecked.lastUpdated < (new Date().getTime() / 1000) - dataSourceUpdateTime[marketDataLastChecked.dataSourceName]){
-                //If the lastupdated time is to out of date run an async to update it
-                //console.log("Updated database: " + marketDataLastChecked.dataSourceName)
-                getMarketData(marketData, marketDataLastChecked.dataSourceName,'usd');
-            }
-            //average the price
-            let aggregate = {}
-            for (const [key, value] of Object.entries(marketDataLastChecked.data)) {
-                for(const [ticker, tickerPrice] of Object.entries(marketDataLastChecked.data[key])){
-                    if(aggregate[ticker]){
-                        aggregate[ticker].push(tickerPrice)
-                    }else{
-                        aggregate[ticker] = []
-                        aggregate[ticker].push(tickerPrice)
-                    }
-                }
-
-            }
-            for (const [key, value] of Object.entries(aggregate)){
-                let jsonFormat = {}
-                jsonFormat.currency = key
-                //filter outliers
-                let outliers = filterOutliers(value)
-                //avg and set the price value
-                jsonFormat.value = parseFloat(average(outliers).toFixed(8))
-                jsonFormat.last_updated = marketDataLastChecked.lastUpdated
-                response.push(jsonFormat)
-            }
-        })
-    }
-    res.json(response)
-});
-
-router.get(ROOT_PREFIX + '/api/v1/price/:currency', async(req,res) =>{
-
-    //create marketData array
-    let marketData = []
-    //load marketData
-    const arrPersistentDataSource = await readDataSource();
-    for (const dataSource of arrPersistentDataSource) {
-        marketData.push(dataSource);
-    }
-
-    //return a single currency
-    let response = []
-    const average = array => array.reduce((a, b) => a + b) / array.length;
-
-    //if marketData isn't set up
-    if(marketData.length == 0){
-        console.log("ran no db")
-        await getMarketData(marketData, 'coinGecko','usd');
-        await getMarketData(marketData, 'coinGeckoDirect', 'usd')
-        await getMarketData(marketData, 'binance','usd');
-        await getMarketData(marketData, 'coinMarketCap','usd');
-    }else{
-        let aggregate = {}
-        let oldestCheck = 0
-        //Look through our db finding anything that matches the currency provided, Take all the values and add them up then return the avg with removed outliers
-        marketData.forEach((marketDataLastChecked) => {
-            if(marketDataLastChecked.lastUpdated < (new Date().getTime() / 1000) - dataSourceUpdateTime[marketDataLastChecked.dataSourceName]){
-                //If the lastupdated time is to out of date run an async to update it
-                console.log("Updated database: " + marketDataLastChecked.dataSourceName)
-                getMarketData(marketData, marketDataLastChecked.dataSourceName,req.params.currency);
-            }
-            if (marketDataLastChecked.lastUpdated > oldestCheck){oldestCheck = marketDataLastChecked.lastUpdated}
-            //average the price
-
-            for (const [key, value] of Object.entries(marketDataLastChecked.data)) {
-                for(const [ticker, tickerPrice] of Object.entries(marketDataLastChecked.data[key])){
-                    if(ticker == req.params.currency){
-                        if(aggregate[ticker]){
-                            aggregate[ticker].push(tickerPrice)
-                        }else{
-                            aggregate[ticker] = []
-                            aggregate[ticker].push(tickerPrice)
-                        }
-                    }
-                }
-            }
-        })
-        for (const [key, value] of Object.entries(aggregate)){
-
-            let jsonFormat = {}
-            jsonFormat.currency = key
-            //filter outliers
-            let outliers = filterOutliers(value)
-            //avg and set the price value
-            jsonFormat.value = parseFloat(average(outliers).toFixed(8))
-            jsonFormat.last_updated = oldestCheck
-    
-            response.push(jsonFormat)
+    // Aggregate the prices from our various sources
+    const arrResponse = [];
+    arrMarketData.forEach((marketDataLastChecked) => {
+        if (marketDataLastChecked.lastUpdated < (new Date().getTime() / 1000) - dataSourceUpdateTime[marketDataLastChecked.dataSourceName]) {
+            // Start a data refresh if this looks outdated
+            getMarketData(arrMarketData, marketDataLastChecked.dataSourceName, 'usd');
         }
-    }
-    res.json(response)
+
+        // Save all instances of price data
+        const aggregate = {}
+        for (const [key, value] of Object.entries(marketDataLastChecked.data)) {
+            for(const [ticker, tickerPrice] of Object.entries(marketDataLastChecked.data[key])){
+                if (aggregate[ticker]) {
+                    aggregate[ticker].push(tickerPrice);
+                } else {
+                    aggregate[ticker] = [];
+                    aggregate[ticker].push(tickerPrice);
+                }
+            }
+        }
+
+        // Average and format
+        for (const [strCurrency, nPrice] of Object.entries(aggregate)) {
+            arrResponse.push({
+                currency: strCurrency,
+                // Perform outlier filtering, averaging, then rounding
+                value: parseFloat(average(filterOutliers(nPrice)).toFixed(8)),
+                last_updated: marketDataLastChecked.lastUpdated
+            });
+        }
+    });
+    res.json(arrResponse);
 });
+
+router.get(ROOT_PREFIX + '/api/v1/price/:currency', async (req, res) => {
+    // Require a currency parameter
+    if (!req.params.currency) return res.status(400).json({ err: 'The "currency" parameter is missing!'});
+    const strCurrency = req.params.currency.toLowerCase();
+
+    // Fetch market data from disk
+    const arrMarketData = await prepareMarketData();
+    if (arrMarketData.length == 0) {
+        console.error('Warning: Oracle has no data after multiple attempts, cannot provide data to API requests!');
+        return res.status(500).json({ err: "Oracle doesn't have enough data to respond, try again later!" });
+    }
+
+    // Aggregate the prices from our various sources
+    const arrAggregatedPrices = [];
+    let nOldestCheck = 0;
+    arrMarketData.forEach((cMarketDataLastChecked) => {
+        if (cMarketDataLastChecked.lastUpdated < (new Date().getTime() / 1000) - dataSourceUpdateTime[cMarketDataLastChecked.dataSourceName]) {
+            // Start a data refresh if this looks outdated
+            console.log("Updated database: " + cMarketDataLastChecked.dataSourceName);
+            getMarketData(arrMarketData, cMarketDataLastChecked.dataSourceName, strCurrency);
+        }
+        if (cMarketDataLastChecked.lastUpdated > nOldestCheck) nOldestCheck = cMarketDataLastChecked.lastUpdated;
+
+        // Save all instances of price data for this currency
+        for (const [key, value] of Object.entries(cMarketDataLastChecked.data)) {
+            for(const [ticker, tickerPrice] of Object.entries(cMarketDataLastChecked.data[key])){
+                if (ticker === strCurrency) {
+                    arrAggregatedPrices.push(tickerPrice);
+                }
+            }
+        }
+    });
+
+    // Check we have any relevent data
+    if (arrAggregatedPrices.length == 0) return res.status(400).json({ err: `The currency "${strCurrency}" either doesn't exist, or Oracle does not yet have any data for it.` });
+
+    // Return the data!
+    res.json({
+        currency: strCurrency,
+        // Perform outlier filtering, averaging, then rounding
+        value: parseFloat(average(filterOutliers(arrAggregatedPrices)).toFixed(8)),
+        last_updated: nOldestCheck
+    });
+});
+
+const average = array => array.reduce((a, b) => a + b) / array.length;
+
+async function prepareMarketData() {
+    // Fetch market data from disk
+    let arrMarketData = await readDataSource();
+
+    // If there's no market data, we'll fetch it real quick before attempting to respond
+    if (arrMarketData.length == 0) {
+        console.warn("Price API called without any data in DB, fetching from all sources...");
+        await getMarketData(arrMarketData, 'coinGecko','usd');
+        await getMarketData(arrMarketData, 'coinGeckoDirect', 'usd')
+        await getMarketData(arrMarketData, 'binance','usd');
+        await getMarketData(arrMarketData, 'coinMarketCap','usd');
+        arrMarketData = await readDataSource();
+    }
+
+    return arrMarketData;
+}
 
 module.exports = router;
