ddd40ea7800172bf1e27f20b7e65a8707c28605a
JSKitty <jskitty@protonmail.com>
2024-04-16 21:05:25 +0100

    Adapt hrefs depending on the host paths

--- public/js/prices.js
+++ public/js/prices.js
@@ -13,6 +13,22 @@ if (afterDomain === "oracle") {
     baseURL = `/${afterDomain}`;
 }
 
+// Get all links
+const links = document.querySelectorAll('a');
+
+// Regex that tests if URL is not external
+const regex = new RegExp('^(http://|https://|ftp://|\/\/)');
+
+// Iterate over each link and replace href if not external
+links.forEach(link => {
+    const originalHref = link.getAttribute('href');
+    // Check if originalHref contains baseURL and is not external
+    if (!originalHref.startsWith(baseURL) && !regex.test(originalHref)) {
+        // Prepend baseURL to the original href only when it's not already there and not an external URL
+        link.setAttribute('href', `${baseURL}${originalHref}`);
+    }
+});
+
 
 let arrPrices = [];
 
